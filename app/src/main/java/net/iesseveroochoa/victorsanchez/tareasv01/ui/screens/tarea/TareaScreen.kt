7efd8a4fea98f2087a7beccf6bef1b08aeacfd90
vagosu <vagos11234@gmail.com>
2024-11-07 20:29:37 +0100

    fin

--- app/src/main/java/net/iesseveroochoa/victorsanchez/tareasv01/ui/screens/tarea/TareaScreen.kt
+++ app/src/main/java/net/iesseveroochoa/victorsanchez/tareasv01/ui/screens/tarea/TareaScreen.kt
@@ -101,6 +101,7 @@ fun taskScreen(
                 onClick = { // Cuando se hace clic en el botón flotante se llama a la función onGuardar
                     if (uiStateTarea.esFormularioValido) {
                         viewModel.onGuardar()
+                        onNavigateBack()
                     } else {
                         uiStateTarea.scope.launch {
                             uiStateTarea.snackbarHostState.showSnackbar(
