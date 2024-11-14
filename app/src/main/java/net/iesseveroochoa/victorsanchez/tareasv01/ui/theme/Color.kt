8893039127c88cc82e77d2641e7e1cab6a2bb684
vagosu <vagos11234@gmail.com>
2024-11-14 20:36:46 +0100

    Opcional 2

--- app/src/main/java/net/iesseveroochoa/victorsanchez/tareasv01/ui/theme/Color.kt
+++ app/src/main/java/net/iesseveroochoa/victorsanchez/tareasv01/ui/theme/Color.kt
@@ -1,15 +1,34 @@
 package net.iesseveroochoa.victorsanchez.tareasv01.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
+/*
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PurpleGrey40 = Color(0xFF625b
+71)
+ */
+// val Pink40 = Color(0xFF7D5260)
 
 val ColorPrioridadAlta = Color(0xFFFF0000)  // Rojo
-val ColorPrioridadMedia = Color(0xFFFFA500) // Naranja
-val ColorPrioridadBaja = Color(0xFF008000)  // Verde
+
+val light_primary = Color(0xFF6750A4)
+val light_onPrimary = Color(0xFFFFFFFF)
+val light_primaryContainer = Color(0xFFEADDFF)
+val light_onPrimaryContainer = Color(0xFF21005D)
+val light_secondary = Color(0xFF625B71)
+val light_onSecondary = Color(0xFFFFFFFF)
+val light_secondaryContainer = Color(0xFFE8DEF8)
+val light_onSecondaryContainer = Color(0xFF1D192B)
+val light_tertiary = Color(0xFF7D5260)
+val light_onTertiary = Color(0xFFFFFFFF)
+val light_tertiaryContainer = Color(0xFFFFD8E4)
+val light_onTertiaryContainer = Color(0xFF31111D)
+val light_error = Color(0xFFB3261E)
+val light_errorContainer = Color(0xFFF9DEDC)
+val light_onError = Color(0xFFFFFFFF)
+val light_onErrorContainer = Color(0xFF410E0B)
+
+
--- app/src/main/java/net/iesseveroochoa/victorsanchez/tareasv01/ui/theme/Theme.kt
+++ app/src/main/java/net/iesseveroochoa/victorsanchez/tareasv01/ui/theme/Theme.kt
@@ -1,58 +1,53 @@
 package net.iesseveroochoa.victorsanchez.tareasv01.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.Shapes
+
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val LightColors = lightColorScheme(
+    primary = light_primary,
+    onPrimary =light_onPrimary,
+    primaryContainer = light_primaryContainer,
+    onPrimaryContainer = light_onPrimaryContainer,
+    secondary = light_secondary,
+    onSecondary = light_onSecondary,
+    secondaryContainer = light_secondaryContainer,
+    onSecondaryContainer = light_onSecondaryContainer,
+    tertiary = light_tertiary,
+    onTertiary = light_onTertiary,
+    tertiaryContainer = light_tertiaryContainer,
+    onTertiaryContainer = light_onTertiaryContainer,
+    error = light_error,
+    errorContainer = light_errorContainer,
+    onError = light_onError,
+    onErrorContainer = light_onErrorContainer,
 )
 
 @Composable
 fun TareasV01Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    useDarkTheme: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
+    val Shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(8.dp),
+        large = RoundedCornerShape(16.dp)
     )
+
+
+    val DarkColors = null
+    val colors = if (useDarkTheme) DarkColors else LightColors
+    if (colors != null) {
+        MaterialTheme(
+            colorScheme = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
